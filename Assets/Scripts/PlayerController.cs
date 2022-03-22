e173f47d3ab8ccecfd9c1fbf66961efb93a1de29
Marcelo Carlos <marcelocarlosjr@hotmail.com>
2022-03-22 16:21:53 -0400

    Movement + Rotation
    Rotation (WIP)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,47 @@ using NETWORK_ENGINE;
 
 public class PlayerController : NetworkComponent
 {
+    public Rigidbody2D MyRig;
+
     public Vector2 MoveInput;
+    public Vector2 AimInput;
+
+    public Vector2 AimDir;
+
+    public float MoveSpeed;
     public override void HandleMessage(string flag, string value)
     {
-        
+        if(flag == "MOVE" && IsServer)
+        {
+            MoveInput = VectorFromString(value);
+        }
+        if(flag == "AIM" && IsServer)
+        {
+            AimDir = VectorFromString(value);
+        }
     }
 
     public override void NetworkedStart()
     {
         
+    }
+
+    public static Vector2 VectorFromString(string value)
+    {
+        char[] temp = { '(', ')' };
+        string[] args = value.Trim(temp).Split(',');
+        return new Vector2(float.Parse(args[0].Trim()), float.Parse(args[1].Trim()));
+    }
+
+    public Vector2 GlobalMousePos(Vector2 position)
+    {
+        Vector3 mousePos = new Vector3(position.x, position.y, Mathf.Infinity);
+
+        Ray rayPos = Camera.main.ScreenPointToRay(mousePos);
+
+        Vector2 relative = (rayPos.origin - MyRig.transform.position).normalized;
+
+        return relative;
     }
 
     public override IEnumerator SlowUpdate()
@@ -23,9 +55,21 @@ public class PlayerController : NetworkComponent
     }
     public void OnMoveInput(InputAction.CallbackContext context)
     {
-        MoveInput = context.ReadValue<Vector2>();
-        SendCommand("MOVE", MoveInput.ToString());
+        if (IsLocalPlayer)
+        {
+            MoveInput = context.ReadValue<Vector2>();
+            SendCommand("MOVE", MoveInput.ToString());
+        }
     }
+    public void OnAimInput(InputAction.CallbackContext context)
+    {
+        if (IsLocalPlayer)
+        {
+            AimInput = context.ReadValue<Vector2>();
+            SendCommand("AIM", GlobalMousePos(AimInput).ToString());
+        }
+    }
+    
     public void OnLClickInput(InputAction.CallbackContext context)
     {
 
@@ -37,5 +81,21 @@ public class PlayerController : NetworkComponent
     public void OnSprintInput(InputAction.CallbackContext context)
     {
 
+    }
+    private void Start()
+    {
+        MyRig = GetComponent<Rigidbody2D>();
+        if(MyRig == null)
+        {
+            throw new System.Exception("ERROR: Could not find Rigidbody!");
+        }
+    }
+    private void Update()
+    {
+        if (IsServer)
+        {
+            MyRig.velocity = (MoveInput * MoveSpeed);
+            MyRig.transform.up = AimDir;
+        }
     }
 }
