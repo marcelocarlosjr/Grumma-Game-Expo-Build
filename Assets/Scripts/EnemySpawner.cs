c706b0cffc168ffd2d07f7bea309296e02863f39
Marcelo Carlos <marcelocarlosjr@hotmail.com>
2022-04-22 01:41:29 -0400

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -23,14 +23,37 @@ public class EnemySpawner : MonoBehaviour
         {
             if (!LinkedEnemy)
             {
-                StartCoroutine(SpawnEnemy());
+                if (!CheckingForPlayer && !Spawning)
+                {
+                    StartCoroutine(CheckPlayerProximity());
+                }
             }
         }
     }
-
+    bool Spawning;
     public IEnumerator SpawnEnemy()
     {
-        yield return new WaitForSeconds(1);
+        Spawning = true;
+        FindObjectOfType<NetworkCore>().NetCreateObject(SpawnPrefab, -1, this.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(RespawnTimer);
+        Spawning = false;
+    }
+    bool CheckingForPlayer;
+    public IEnumerator CheckPlayerProximity()
+    {
+        CheckingForPlayer = true;
+        foreach(PlayerController pc in FindObjectsOfType<PlayerController>())
+        {
+            if(Vector3.Distance(this.transform.position, pc.transform.position) < 15)
+            {
+                if (!Spawning)
+                {
+                    StartCoroutine(SpawnEnemy());
+                }
+            }
+        }
+        yield return new WaitForSeconds(2);
+        CheckingForPlayer = false;
     }
 
 
